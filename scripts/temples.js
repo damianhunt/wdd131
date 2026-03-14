d355119a3460826daf394a04c3c71b1591dde462
damianhunt <dhuntsa@gmail.com>
2026-03-15 01:03:39 +0200

    started week 02 assignment and basic css

---
+++ scripts/temples.js
@@ -0,0 +1,9 @@
+// hamburger
+function toggleMenu() {
+    document.querySelector(".primaryNav").classList.toggle("open");
+    document.querySelector(".hamburgerBtn").classList.toggle("open");
+
+}
+
+const x = document.querySelector('.hamburgerBtn')
+x.onclick = toggleMenu;
