28d19f559c0c167f32e4cf480fea3eafe4270360
damianhunt <dhuntsa@gmail.com>
2026-04-09 22:14:53 +0200

    getting there

--- project/scripts/functions.js
+++ project/scripts/functions.js
@@ -6,3 +6,18 @@ hambutton.addEventListener('click', () => {
 	mainnav.classList.toggle('show');
 	hambutton.classList.toggle('show');
 });
+
+//local storage contact counter
+
+window.addEventListener("load", () => {
+  // Retrieve existing count or default to 0
+  let contactedCount = Number(localStorage.getItem("contacted-counter")) || 0;
+  
+  // Increment the counter
+  contactedCount++;
+  
+  // Store the updated count back to localStorage
+  localStorage.setItem("contacted-counter", contactedCount);
+  
+  console.log(`Total customers made contact: ${contactedCount}`);
+});
